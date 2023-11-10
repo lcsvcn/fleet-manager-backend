41ecc24a1c91b84187a5e26762948e62fbcbdbd4
Lucas Vieira <6011385+lcsvcn@users.noreply.github.com>
2023-11-10 19:02:41 -0300

    Add return statements to API endpoints

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/api/v1/fleets', (req, res) => {
     number_drones: fleet.drones.length,
   }));
 
-  res.json(fleetsWithNumberDrones);
+  return res.status(200).json(fleetsWithNumberDrones);
 });
 
 app.post("/api/v1/fleets", (req, res) => {
@@ -34,7 +34,7 @@ app.post("/api/v1/fleets", (req, res) => {
 
   newFleet.drones = [];
   fleetData.push(newFleet);
-  res.status(201).json(newFleet);
+  return res.status(200).json(newFleet);
 });
 
 app.get('/api/v1/fleet/:id', (req, res) => {
@@ -81,20 +81,7 @@ app.post("/api/v1/fleet/:id/drones", (req, res) => {
   }
 
   foundFleet.drones.push(newDrone);
-  res.status(201).json(newDrone);
-});
-
-app.delete("/api/v1/fleet/:id/drones/:drone_id", (req, res) => {
-  const { id, drone_id } = req.params;
-
-  const foundFleet = fleetData.find((fleet) => fleet.fleet_id === id);
-
-  if (!foundFleet) {
-    return res.status(404).json({ error: 'Fleet not found' });
-  }
-
-  foundFleet.drones = foundFleet.drones.filter((drone) => drone.drone_id !== drone_id);
-  res.json({ message: "Drone deleted from fleet" });
+  return res.status(200).json(newDrone);
 });
 
 app.get('/api/v1/clients', (req, res) => {
@@ -111,7 +98,7 @@ app.get('/api/v1/clients', (req, res) => {
     };
   });
 
-  res.json(clientsWithCounts);
+  return res.status(200).json(clientsWithCounts);
 });
 
 
@@ -132,7 +119,7 @@ app.post("/api/v1/clients", (req, res) => {
 
   const newClient = { client_email, client_password, first_name, last_name };
   clientData.push(newClient);
-  res.status(200).json(newClient);
+  return res.status(200).json(newClient);
 });
 
 
@@ -149,7 +136,7 @@ app.post('/api/v1/login', (req, res) => {
     return res.status(401).json({ error: 'Incorrect password' });
   }
 
-  res.status(200).json({ message: 'Login successful' });
+  return res.status(200).json({ message: 'Login successful' });
 });
 
 app.listen(port, () => {

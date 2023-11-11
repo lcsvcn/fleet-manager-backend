eed16e1420307afd42fe4ec3d082e826121d5898
Lucas Vieira <6011385+lcsvcn@users.noreply.github.com>
2023-11-10 22:25:24 -0300

    Update fleetData object and add owner_email validation

--- index.js
+++ index.js
@@ -6,13 +6,20 @@ const cors = require("cors");
 
 app.use(express.json());
 
-app.use(cors({credentials: true, origin: true}));
+app.use(cors({ credentials: true, origin: true }));
 
-let fleetData = [];
+let fleetData = {};
 let clientData = [];
 
 app.get('/api/v1/fleets', (req, res) => {
-  const fleetsWithNumberDrones = fleetData.map((fleet) => ({
+  const { owner_email } = req.headers;
+
+  if (!owner_email) {
+    return res.status(400).json({ error: "Bad Request: Owner_email not provided" });
+  }
+
+  const userFleetData = fleetData[owner_email] || [];
+  const fleetsWithNumberDrones = userFleetData.map((fleet) => ({
     ...fleet,
     number_drones: fleet.drones.length,
   }));
@@ -23,24 +30,28 @@ app.get('/api/v1/fleets', (req, res) => {
 app.post("/api/v1/fleets", (req, res) => {
   const newFleet = req.body;
 
-  if (!newFleet || !newFleet.fleet_id || !newFleet.fleet_name) {
+  if (!newFleet || !newFleet.fleet_id || !newFleet.fleet_name || !newFleet.owner_email) {
     return res.status(400).json({ error: "Bad Request: Invalid or missing data" });
   }
 
-  const existingFleet = fleetData.find((fleet) => fleet.fleet_id === newFleet.fleet_id);
+  const userFleetData = fleetData[newFleet.owner_email] || [];
+  const existingFleet = userFleetData.find((fleet) => fleet.fleet_id === newFleet.fleet_id);
+
   if (existingFleet) {
-    return res.status(409).json({ error: "Conflict: Fleet ID already exists" });
+    return res.status(409).json({ error: "Conflict: Fleet ID already exists for this owner" });
   }
 
   newFleet.drones = [];
-  fleetData.push(newFleet);
+  userFleetData.push(newFleet);
+  fleetData[newFleet.owner_email] = userFleetData; // Update fleetData for the owner_email
   return res.status(200).json(newFleet);
 });
 
 app.get('/api/v1/fleet/:id', (req, res) => {
   const { id } = req.params;
+  const { owner_email } = req.headers;
 
-  const foundFleet = fleetData.find((fleet) => fleet.fleet_id === id);
+  const foundFleet = fleetData[owner_email] && fleetData[owner_email].find((fleet) => fleet.fleet_id === id);
 
   if (foundFleet) {
     return res.status(200).json(foundFleet);
@@ -51,8 +62,9 @@ app.get('/api/v1/fleet/:id', (req, res) => {
 
 app.get('/api/v1/fleet/:id/drones', (req, res) => {
   const { id } = req.params;
+  const { owner_email } = req.headers;
 
-  const foundFleet = fleetData.find((fleet) => fleet.fleet_id === id);
+  const foundFleet = fleetData[owner_email] && fleetData[owner_email].find((fleet) => fleet.fleet_id === id);
 
   if (foundFleet) {
     return res.status(200).json(foundFleet.drones);
@@ -63,16 +75,17 @@ app.get('/api/v1/fleet/:id/drones', (req, res) => {
 
 app.post("/api/v1/fleet/:id/drones", (req, res) => {
   const { id } = req.params;
+  const { owner_email } = req.headers;
   const newDrone = req.body;
 
-  const foundFleet = fleetData.find((fleet) => fleet.fleet_id === id);
+  const foundFleet = fleetData[owner_email] && fleetData[owner_email].find((fleet) => fleet.fleet_id === id);
 
   if (!foundFleet) {
     return res.status(404).json({ error: 'Fleet not found' });
   }
 
-  if (!newDrone || !newDrone.drone_id || !newDrone.drone_name) {
-    return res.status(400).json({ error: "Bad Request: Invalid or missing data" });
+  if (!newDrone || !newDrone.drone_id || !newDrone.drone_name || newDrone.owner_email !== owner_email) {
+    return res.status(400).json({ error: "Bad Request: Invalid or missing data, or incorrect owner_email" });
   }
 
   const existingDrone = foundFleet.drones.find((drone) => drone.drone_id === newDrone.drone_id);
@@ -86,7 +99,7 @@ app.post("/api/v1/fleet/:id/drones", (req, res) => {
 
 app.get('/api/v1/clients', (req, res) => {
   const clientsWithCounts = clientData.map((client) => {
-    const clientFleets = fleetData.filter((fleet) => fleet.owner_email === client.client_email);
+    const clientFleets = fleetData[client.client_email] || [];
     const numberFleets = clientFleets.length;
     
     const numberDrones = clientFleets.reduce((acc, fleet) => acc + fleet.drones.length, 0);
@@ -100,7 +113,6 @@ app.get('/api/v1/clients', (req, res) => {
 
   return res.status(200).json(clientsWithCounts);
 });
-
 
 app.post("/api/v1/clients", (req, res) => {
   const { client_email, client_password, first_name, last_name } = req.body;
@@ -122,7 +134,6 @@ app.post("/api/v1/clients", (req, res) => {
   return res.status(200).json(newClient);
 });
 
-
 app.post('/api/v1/login', (req, res) => {
   const { client_email, client_password } = req.body;
 
@@ -142,4 +153,3 @@ app.post('/api/v1/login', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
-
